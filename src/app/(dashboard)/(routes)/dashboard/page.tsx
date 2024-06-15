e292dabd3adc1b6bfb0e4b0adfa1dfd31904e239
sandeep0009 <500sandeepks@gmail.com>
2024-06-15 19:48:29 +0530

    set up folder setructure

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const Dasboard = () => {
+  return (
+    <div>Dasboard</div>
+  )
+}
+
+export default Dasboard
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+
+"use client"
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>page (Unprotected)</div>
+  )
+}
+
+export default LandingPage
